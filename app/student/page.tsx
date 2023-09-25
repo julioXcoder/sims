6974932c590a1a5eed7391e0d529f3d972d22294
Julio Oran Njeza <julioxcoder@gmail.com>
2023-09-25 22:34:31 +0300

    Add Application Schema

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -4,8 +4,8 @@ const StudentPage = () => {
   return (
     <div>
       <div className="tabs">
-        <a className="tab tab-lifted">Exams</a>
-        <a className="tab tab-lifted tab-active">Finals</a>
+        <a className="tab tab-lifted">CA</a>
+        <a className="tab tab-active tab-lifted">Finals</a>
         <a className="tab tab-lifted">SUPP</a>
         <a className="tab tab-lifted">Retake</a>
       </div>
